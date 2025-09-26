bae924bb18e0ee779dfff208a3d835bb3b276c4f
RizkiyanaPutri <rizkiyanaputri8e30@gmail.com>
2025-09-26 16:53:44 +0700

    membuat repository baru

--- Test.java
+++ Test.java
@@ -1,5 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hai, ini baris kode pertama");
+        System.out.println("Hi, ini baris kode pertama");
     }
 }
