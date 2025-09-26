655d7df89b0243779764045021b1f8c169f861bd
RizkiyanaPutri <rizkiyanaputri8e30@gmail.com>
2025-09-26 17:27:04 +0700

    Rizkiyana memodifikasi kode

--- Test.java
+++ Test.java
@@ -1,5 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hi, ini baris kode pertama");
+        System.out.println("Hi, ini baris kode pertama yang dimodifikasi user 1 (RizkiyanaPutri)");
+        System.out.println("user 1 menambahkan baris baru");
     }
 }
