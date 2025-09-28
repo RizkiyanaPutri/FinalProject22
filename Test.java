947d4cbd37481ba43471ee8804ae0f80bb481c2f
callistakushermyla731 <callistakushermyla731@gmail.com>
2025-09-28 21:26:46 +0700

    Callista memodifikasi program

--- Test.java
+++ Test.java
@@ -2,5 +2,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hi, ini baris kode pertama yang dimodifikasi user 1 (RizkiyanaPutri)");
         System.out.println("user 1 menambahkan baris baru");
+        System.out.println("Hi, ini baris kode pertama yang dimodifikasi user 2 (CallistaKushermyla)");
+        System.out.println("user 2 menambahkan baris baru");
     }
 }
